22d97cdb4d91764fbdaffbc357e8de23ed24ef06
0APPTI0 <2458530429@qq.com>
2018-12-11 21:04:56 +0800

    实现了NBCD 8421 的算法

---
+++ BCD/src/BCD.java
@@ -0,0 +1,28 @@
+public class BCD {
+
+
+    Number num1 = new Number("111");
+    Number num2 = new Number("111");
+
+    public Number ADDITION(Number a1, Number a2){
+        return a1.ADD(a1,a2);
+    }
+
+
+    //这里只做正数之间的减法
+    public Number SUBTION(Number a1 , Number a2){
+        //a2先每一位加6取反
+        a2 = a2.Handle(a2);
+        //然后做加法
+        a1 = a1.ADD(a1,a2);
+        //然后检查结果
+        if (a1.bitCarry){
+            return a1;
+        }
+        else {
+            return a1.reverse(a1);
+        }
+    }
+
+
+}
---
+++ BCD/src/Number.java
@@ -0,0 +1,147 @@
+import java.util.ArrayList;
+
+public class Number {
+
+    int defaultLength = 10;
+
+    public String positive = "0000";
+
+    public String negative = "0001";
+
+    //表示进位的符号，用完之后要手动设为false
+    public boolean bitCarry = false;
+
+    public String symbol ;
+
+    public String[] Absolute_value  = new String[defaultLength];
+
+    //初始化方法
+    public Number(String a){
+        String temp;
+        if (a.charAt(0) == '-'){
+            this.symbol = negative;
+            temp = a.substring(1);
+        }
+        else if (a.charAt(0) == '+'){
+            temp = a.substring(1);
+        }
+        else {
+            this.symbol = positive;
+            temp = a;
+        }
+
+        this.defaultLength = temp.length();
+
+        char[] numbers = temp.toCharArray();
+
+        for (int i = 0 ; i < numbers.length ; i ++){
+            int Ibit = numbers[i] - '0';
+            String bit = Integer.toBinaryString(Ibit);
+            Absolute_value[i] = bit;
+        }
+
+    }
+
+    //加法，针对十进制的每一位
+    public String Addition(String a , String b){
+        char[] a1 = a.toCharArray();
+        char[] b1 = b.toCharArray();
+        int[] A = new int[a.length()];
+        int[] B = new int[b.length()];
+        for (int i = 0 ; i < a.length() ; i++){
+            A[i] = a1[i] - '0';
+            B[i] = b1[i] - '0';
+        }
+
+        int JiLuJingWei = 0;
+        int[] result = new int[a.length()];
+        for (int count = a.length() - 1 ; count >=0 ; count --){
+            //三个中有两个1，产生了进位
+            if ((A[count] == 1 && B[count] == 1) || (A[count] == 1 && JiLuJingWei == 1) || (B[count] == 1 && JiLuJingWei == 1) ){
+                result[count] = A[count] + B[count] + JiLuJingWei - 2;
+                JiLuJingWei = 1;
+            }
+            else {
+                result[count] = A[count] + B[count] + JiLuJingWei;
+                JiLuJingWei = 0;
+            }
+        }
+        if (JiLuJingWei == 1){
+            this.bitCarry = true;
+        }
+        return result.toString();
+    }
+
+    //减法,在reverse里面会用到
+    public String Subtraction(String a , String b){
+        int[] temp = new int[a.length()];
+        for (int i = 0 ; i < temp.length ; i++){
+            temp[i] = 0;
+        }
+        temp[temp.length - 1] = 1;
+        char[] C = new char[b.length()];
+        char[] B = b.toCharArray();
+        for (int i = 0 ; i < B.length ; i ++){
+            if (B[i] == '1'){
+                C[i] = '0';
+            }
+            else C[i] = '1';
+        }
+        return Addition(C.toString(),temp.toString());
+    }
+
+
+
+    //数与数之间的加法.最终结果加到a1上面
+    public Number ADD(Number a1 , Number a2){
+        for (int i = a1.Absolute_value.length - 1 ; i >=0 ; i --){
+            if (a1.bitCarry){
+                a1.bitCarry = false;
+                a1.Absolute_value[i] = a1.Addition(a1.Absolute_value[i],"0001");
+                a1.Absolute_value[i] = a1.Addition(a1.Absolute_value[i],a2.Absolute_value[i]);
+            }
+            else {
+                a1.Absolute_value[i] = a1.Addition(a1.Absolute_value[i],a2.Absolute_value[i]);
+            }
+        }
+        return a1;
+    }
+
+    //所谓取反加一
+    public Number reverse(Number n1){
+        //取反
+        for (int i = 0 ; i < n1.Absolute_value.length ; i++){
+            n1.Absolute_value[i] = n1.Subtraction("1001",n1.Absolute_value[i]);
+        }
+        Number zore = new Number("0");
+        String[] temp = new String[n1.Absolute_value.length];
+        for (int i = 0 ; i < temp.length - 1 ; i ++){
+            temp[i] = "0000";
+        }
+        temp[temp.length - 1] = "0001";
+        zore.Absolute_value = temp;
+        //加一
+        return n1.ADD(n1,zore);
+    }
+
+    //加6（"0110"）按位取反
+    public Number Handle(Number a){
+        for (int i = 0 ; i < a.Absolute_value.length - 1 ; i ++){
+            Absolute_value[i] = Addition(Absolute_value[i],"0110");
+            char[] temp = Absolute_value[i].toCharArray();
+            for (char x:temp){
+                if (x == '0'){
+                    x = '1';
+                }
+                else x = '0';
+            }
+            Absolute_value[i] = temp.toString();
+        }
+        //为了避免在这里产生进位方面的影响
+        a.bitCarry = false;
+
+        return a;
+
+    }
+
+}
