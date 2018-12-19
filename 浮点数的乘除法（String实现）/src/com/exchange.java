deef8e834b2382d1af77d1819126b7c3dbb0f34f
0APPTI0 <2458530429@qq.com>
2018-12-19 14:33:10 +0800

    完成了浮点数与小数的转换实现

--- 浮点数的乘除法（String实现）/src/com/exchange.java
+++ 浮点数的乘除法（String实现）/src/com/exchange.java
@@ -1,16 +1,65 @@
 package com;
 
+
 public class exchange {
-    //实现浮点数与小数相互转换的类
 
-    //小数转变为浮点数
-    public String num_to_float(String num){
-
-        return "";
+    //把输入的字符串分成整数部分s1和小数部分s2,最终得到一个浮点数
+    public String strToDou(String s){
+        int n = s.indexOf(".");
+        if(n<0){
+            s += ".0";
+            n = s.indexOf(".");
+        }
+        String s1 = s.substring(0,n);
+        String s2 = s.substring(n+1);
+        double a = sToInt(s1) + sToDou(s2);
+        return String.valueOf(a);
     }
 
-    //浮点数转变为小数
-    public String float_to_num(String Float){
-        return "";
+    //把整数部分转成double
+    //递归实现
+    public int sToInt(String s){
+        if(s.length()==1) return s.charAt(0)-'0';
+        return sToInt(s.substring(0,s.length()-1)) * 2 + (s.charAt(s.length()-1)-'0');
     }
+
+    //把小数部分转成double
+    public double sToDou(String s){
+        if(s.length()==1) return (s.charAt(0)-'0')/2.0;
+        return (s.charAt(0)-'0')/2.0 + sToDou(s.substring(1))/2.0;
+    }
+
+
+    //把输入的浮点数分成整数部分和小数部分，最终得到一个二进制字符串
+    public String douToStr(String s){
+        int n = s.indexOf(".");
+        if(n<0){
+            s = s + ".0";
+            n = s.indexOf(".");
+        }
+        String s1 = s.substring(0,n);
+        String s2 = "0" + s.substring(n);
+
+        String s3 = intToStr(Integer.parseInt(s1));
+        String s4 = dToStr(Double.parseDouble(s2));
+        return  (s3.length()==0? 0 : s3) + "." + s4;
+    }
+
+    //把整数部分转成二进制
+    public String intToStr(int n){
+        if(n==0) return "";
+        int a = n % 2;
+        int b = n / 2;
+        return intToStr(b) + a;
+    }
+
+    //把小数部分转成二进制
+    public String dToStr(double d){
+        if(d-(int)d<0.01) return "" + (int)d;
+        int n = (int)(d * 2);
+        double a = d * 2 - n;
+        return "" + n + dToStr(a);
+    }
+
+
 }
