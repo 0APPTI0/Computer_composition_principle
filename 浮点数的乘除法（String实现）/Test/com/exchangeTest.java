047d4807e79055082ad0001f1b07b567b59777cb
0APPTI0 <2458530429@qq.com>
2018-12-19 18:36:36 +0800

    用普通的迭代实现了浮点数和二进制字符串的互相转换

--- 浮点数的乘除法（String实现）/Test/com/exchangeTest.java
+++ 浮点数的乘除法（String实现）/Test/com/exchangeTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class exchangeTest {
 
+    exchange ec = new exchange();
     @Test
     void strToDou() {
     }
@@ -28,5 +29,21 @@ class exchangeTest {
 
     @Test
     void dToStr() {
+    }
+
+    @Test
+    void STOINT() {
+        assertEquals(2,ec.STOINT("10"));
+    }
+
+    @Test
+    void math_Exchange() {
+        assertEquals(ec.strToDou("110.11"),ec.Math_Exchange("110.11"));
+    }
+
+
+    @Test
+    void math_Loop() {
+        assertEquals("101.1",ec.Math_Loop("5.5"));
     }
 }
--- 浮点数的乘除法（String实现）/src/com/exchange.java
+++ 浮点数的乘除法（String实现）/src/com/exchange.java
@@ -23,11 +23,44 @@ public class exchange {
         return sToInt(s.substring(0,s.length()-1)) * 2 + (s.charAt(s.length()-1)-'0');
     }
 
+    //可以调用库函数，将无符号的二进制字符串转化为int
+    public int STOINT(String s){
+        s = "0" + s;
+        return Integer.parseInt(s,2);
+    }
+
     //把小数部分转成double
     public double sToDou(String s){
         if(s.length()==1) return (s.charAt(0)-'0')/2.0;
         return (s.charAt(0)-'0')/2.0 + sToDou(s.substring(1))/2.0;
     }
+
+    //迭代实现法
+    public String Math_Exchange(String s){
+        int index = s.indexOf(".");
+        String a = s.substring(0,index);
+        String b = s.substring(index+1);
+        double A = 0.0;
+        for (int i = 0 ; i <= a.length() - 1 ; i ++ ){
+            if (a.charAt(i) == '1') {
+                A = A + Math.pow(2, a.length() - i - 1);
+            }
+        }
+        double B = 0.0;
+        for (int j = 0 ; j < b.length() ; j ++){
+            if (b.charAt(j)== '1') {
+                B = B + Math.pow(2, -(j + 1));
+            }
+        }
+
+        return String.valueOf(A + B);
+    }
+
+
+
+
+
+
 
 
     //把输入的浮点数分成整数部分和小数部分，最终得到一个二进制字符串
@@ -63,5 +96,50 @@ public class exchange {
         return "" + n + dToStr(a);
     }
 
+    //迭代实现法
+    public String Math_Loop(String a){
+        int k = a.indexOf(".");
+        String s1 = a.substring(0,k);//整数部分
+        String s2 = "0" + a.substring(k);//小数部分
+        int N = Integer.parseInt(s1);
+        double n = Double.parseDouble(s2);
+        String L1 = "";
+        String L2 = "";
+        int i = 0;
+        while (true){
+            if (Math.pow(2,i) <= N) {
+                i += 1;
+                continue;
+            }
+            else break;
+        }
+        for (; i >= 0 ; i --){
+            int temp = (int) Math.pow(2,i);
+            if (N >= temp){
+                N = N - temp;
+                L1 = L1 + "1";
+            }
+            else if (!L1.equals("")){
+                L1 = L1 + "0";
+            }
+
+        }
+
+        int counterJudge = 32;
+        while (counterJudge >= 0){
+            n = 2 * n;
+            if (n > 1){
+                L2 = L2+"1";
+                n = n - 1;
+            }
+            else if (n == 1){
+                L2 += "1";
+                break;
+            }
+            else L2 = L2 + "0";
+        }
+
+        return L1+"."+L2;
+    }
 
 }
